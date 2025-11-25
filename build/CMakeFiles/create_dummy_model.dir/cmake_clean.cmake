4fa8e1abb871167d705f8038adce862b2b7275ce
Domado <domado.chen@gmail.com>
2025-11-25 22:28:30 +0800

    Add files via upload

---
+++ build/CMakeFiles/create_dummy_model.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/create_dummy_model.dir/create_dummy_model.cpp.o"
+  "CMakeFiles/create_dummy_model.dir/create_dummy_model.cpp.o.d"
+  "CMakeFiles/create_dummy_model.dir/tensor.cpp.o"
+  "CMakeFiles/create_dummy_model.dir/tensor.cpp.o.d"
+  "CMakeFiles/create_dummy_model.dir/utils.cpp.o"
+  "CMakeFiles/create_dummy_model.dir/utils.cpp.o.d"
+  "create_dummy_model"
+  "create_dummy_model.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/create_dummy_model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
